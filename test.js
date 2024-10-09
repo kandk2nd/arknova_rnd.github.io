89e04d6795371604e9d04aa6c23296f96b83a06a
kandk2nd <kandk2nd@gmail.com>
2024-10-09 23:34:07 +0900

    [update]vanjs対応

--- test.js
+++ test.js
@@ -1,36 +1,30 @@
+const srcs = van.state([1,2,3,4])
+const {div, button, table, thead, tr, th, tbody, td,img} = van.tags
 function test(){
-    let srcs = ["1.png","2.png","3.png","4.png"]
     for(let n=0; n < 5; n++){
-        for(let i = (srcs.length - 1); 0 < i; i--){
+        for(let i = (srcs.val.length - 1); 0 < i; i--){
             let rnd =Math.floor((Math.random()) * (i + 1));
-            let tmp = srcs[i];
-            srcs[i] = srcs[rnd];
-            srcs[rnd] = tmp;
+            let tmp = srcs.val[i];
+            srcs.val[i] = srcs.val[rnd];
+            srcs.val[rnd] = tmp;
         }
     }
-    const img1 = document.getElementById("1");
-    img1.src = "0.png";
-    const img2 = document.getElementById("2");
-    img2.src = srcs[0];
-    const img3 = document.getElementById("3");
-    img3.src = srcs[1];
-    const img4 = document.getElementById("4");
-    img4.src = srcs[2];
-    const img5 = document.getElementById("5");
-    img5.src = srcs[3];
-
+    srcs.val = [...srcs.val]
 }
 
-const {button, table, thead, tr, th, tbody, td} = van.tags
+
+const Header = () => {
+    return div({class:"header"}, "ARK NOVA Randomizer")
+}
 
 const RandTable = () => {
-    return table({rule:"none", class:"dataframe"},
-        thead(
-            tr({style:"text-align: right;"}, 
-                ["5","4","3","2","1"].map(n => th(n))
-            )
-        )
+    return table({onclick: test,class:"main"},
+            tr(
+                ["1","2","3","4","5"].map(n => td(n))
+            ),
+                () => tr(td(img({src:"0.png"})),srcs.val.map(n => td(img({src:n+".png"}))))
     )
 }
+const Randbutton = button({onclick: test})
 
-van.add(document.body, RandTable())
+van.add(document.body, Header(), RandTable(),Randbutton)
