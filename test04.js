addab4e6c5447adbc800e3370b4b889aabb24c36
kandk2nd <kandk2nd@gmail.com>
2025-05-26 15:02:31 +0900

    [update]サンプルマップを横長に変更

--- test04.js
+++ test04.js
@@ -78,9 +78,14 @@ const land_color07 =
         "#222222", // 0
     ];
 const DivList = () => {
-    const items = [1, 2, 3, 4, 5, 6, 7, 0, 2, 4, 6, 1, 3, 5, 6, 5, 4, 3, 2, 1, 0, 5, 5, 2, 5, 4, 6];  /*ここにクラス名を*/
+    const items = [
+        1, 0, 3, 4, 5, 6, 7, 0, 2, 4,
+        6, 0, 3, 5, 6, 5, 4, 0, 2, 1,
+        5, 0, 5, 2, 5, 4, 6, 0, 5, 3,
+        4, 0, 0, 1, 4, 6, 5, 0, 2, 4,
+        5, 7, 6, 5, 2, 7, 4, 0, 3, 6];  /*ここにクラス名を*/
     const divs = [];
-    const MAX_ROW = 3;
+    const MAX_ROW = 10;
     // 色直指定ではなく、7色クラス指定で入れて。色変更をクラス指定で行う気がする。
     // forループでdivを生成
     i = 0
@@ -89,7 +94,8 @@ const DivList = () => {
         tmp = [];
         do {
             if (items[i] == 0) {
-                tmp.push(div({ class: "hexagon", style: () => `background-color:#ffffff;`, onclick: () => base_land.val = 0 }, i++));
+                tmp.push(div({ class: "hexagon", style: () => `background-color:#ffffff;`, onclick: () => base_land.val = 0 }));
+                i++
             } else if (items[i] == 1) {
                 tmp.push(div({ class: "hexagon", style: () => `background-color:${land_color01[base_land.val]};`, onclick: () => base_land.val = 1 }, i++));
             } else if (items[i] == 2) {
