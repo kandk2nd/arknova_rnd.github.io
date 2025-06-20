3d1f44af52770317230f3ab9b2525283e61d90be
kandk2nd <kandk2nd@gmail.com>
2025-06-20 13:59:34 +0900

    [update]derive追加

--- hege.js
+++ hege.js
@@ -1,18 +1,45 @@
-const srcs = van.state([1, 2, 3, 4])
 const { div, select, input, thead, h2, h4, tbody, option, form } = van.tags
-function test() {
-    for (let n = 0; n < 5; n++) {
-        for (let i = (srcs.val.length - 1); 0 < i; i--) {
-            let rnd = Math.floor((Math.random()) * (i + 1));
-            let tmp = srcs.val[i];
-            srcs.val[i] = srcs.val[rnd];
-            srcs.val[rnd] = tmp;
-        }
-    }
-    srcs.val = [...srcs.val]
+const lowA = van.state(1)
+const lowB = van.state(1)
+const lowC = van.state(1)
+const population = van.state(1)
+const mid_population = van.state(1)
+const mid_receivable = van.state(1)
+const receivable = van.state(1)
+const worker_tax = van.derive()
+const mid_tax = van.derive()
+const hi_tax = van.derive()
+function calc_worker_tax() {
+    const table = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    return table[lowA.val - 1][lowB.val - 1] * population.val
+}
+function calc_mid_tax() {
+    const table = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    return table[lowA.val - 1][lowB.val - 1] * population.val
+}
+function calc_hi_tax() {
+    const table = []
+    // 入力値をテーブル行に変換する処理が必要。
+    return table[lowA.val - 1][lowB.val - 1] * population.val
+}
+function calc_mid_tax() {
+    return 1
+}
+function calc_hi_tax() {
+    return 1
 }
 
-
+const items = [10, 20, 30, 40]
 const Header = () => {
     return div({ class: "header" }, "hegeee会計")
 }
@@ -21,19 +48,19 @@ const Main = () => {
         div({ class: "lows" },
             h2("lows"),
             h4("lowA"),
-            select({ name: "lowA", placeholder: "lowA" },
+            select({ oninput: e => lowA.val = e.target.value, name: "lowA", placeholder: "lowA" },
                 option({ value: 1 }, "lowA1"),
                 option({ value: 2 }, "lowA2"),
                 option({ value: 3 }, "lowA3"),
             ),
             h4("lowB"),
-            select({ name: "lowB", placeholder: "lowB" },
+            select({ oninput: e => lowB.val = e.target.value, name: "lowB", placeholder: "lowB" },
                 option({ value: 1 }, "lowB1"),
                 option({ value: 2 }, "lowB2"),
                 option({ value: 3 }, "lowB3"),
             ),
             h4("lowC"),
-            select({ name: "lowC", placeholder: "lowC" },
+            select({ oninput: e => lowC.val = e.target.value, name: "lowC", placeholder: "lowC" },
                 option({ value: 1 }, "lowC1"),
                 option({ value: 2 }, "lowC2"),
                 option({ value: 3 }, "lowC3"),
@@ -41,11 +68,13 @@ const Main = () => {
         ),
         div({ class: "workman" },
             h2("Workman"),
-            h4("peples"),
-            input({ type: "number", placeholder: "人口" })
+            h4("population"),
+            input({ id: "population", type: "number", placeholder: "人口" })
         ),
         div({ class: "middle" },
             h2("middle"),
+            h4("compeny_num"),
+            input({ type: "number", placeholder: "数" }),
             h4("compeny_num"),
             input({ type: "number", placeholder: "数" }),
         ),
