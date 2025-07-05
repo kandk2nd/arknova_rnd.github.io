52ed50a32f133693c6519b0138a921aa7232cd9f
kandk2nd <kadnk2nd.gmail.com>
2025-07-05 14:47:16 +0900

    [uddate]4-5人マップ打ち込み完了

--- terra_map.js
+++ terra_map.js
@@ -77,7 +77,51 @@ const land_color07 =
         "#cc99ff", // 1
         "#006633", // 0
     ];
-const DivList = () => {
+const DivList4 = () => {
+    //1:灰,2:赤,3:黄,4:茶,5:黒,6:青,7:緑の色
+    const items = [
+        7, 0, 6, 5, 4, 2, 1, 0, 6, 5, 4, 3, 0,
+        1, 0, 0, 7, 6, 7, 3, 0, 7, 1, 6, 0, 5,
+        2, 6, 5, 0, 1, 4, 5, 0, 0, 2, 0, 0, 2,
+        3, 1, 2, 0, 3, 2, 0, 5, 0, 0, 4, 1, 4,
+        4, 7, 4, 3, 0, 0, 0, 7, 1, 6, 2, 3, 7,
+        5, 0, 0, 0, 6, 1, 0, 3, 4, 0, 5, 6, 2,
+        6, 0, 4, 5, 0, 7, 4, 0, 0, 0, 0, 0, 0,
+        0, 3, 6, 0, 2, 5, 3, 2, 5, 6, 7, 5, 0,
+        7, 1, 2, 0, 3, 1, 4, 6, 7, 1, 2, 3, 4,
+    ];  /*ここにクラス名を*/
+    const divs = [];
+    const MAX_ROW = 13;
+    // 色直指定ではなく、7色クラス指定で入れて。色変更をクラス指定で行う気がする。
+    // forループでdivを生成
+    i = 0
+    divs.push(div(base_land))
+    while (i < items.length) {
+        tmp = [];
+        do {
+            if (items[i] == 0) {
+                tmp.push(div({ class: "hexagon", style: () => `background-color:#ffffff;`, onclick: () => base_land.val = 0 }, 1 + i++));
+            } else if (items[i] == 1) {
+                tmp.push(div({ class: "hexagon", style: () => `background-color:${land_color01[base_land.val]};`, onclick: () => base_land.val = 1 }, 1 + i++));
+            } else if (items[i] == 2) {
+                tmp.push(div({ class: "hexagon", style: () => `background-color:${land_color02[base_land.val]};`, onclick: () => base_land.val = 2 }, 1 + i++));
+            } else if (items[i] == 3) {
+                tmp.push(div({ class: "hexagon", style: () => `background-color:${land_color03[base_land.val]};`, onclick: () => base_land.val = 3 }, 1 + i++));
+            } else if (items[i] == 4) {
+                tmp.push(div({ class: "hexagon", style: () => `background-color:${land_color04[base_land.val]};`, onclick: () => base_land.val = 4 }, 1 + i++));
+            } else if (items[i] == 5) {
+                tmp.push(div({ class: "hexagon", style: () => `background-color:${land_color05[base_land.val]};`, onclick: () => base_land.val = 5 }, 1 + i++));
+            } else if (items[i] == 6) {
+                tmp.push(div({ class: "hexagon", style: () => `background-color:${land_color06[base_land.val]};`, onclick: () => base_land.val = 6 }, 1 + i++));
+            } else if (items[i] == 7) {
+                tmp.push(div({ class: "hexagon", style: () => `background-color:${land_color07[base_land.val]};`, onclick: () => base_land.val = 7 }, 1 + i++));
+            }
+        } while (i < items.length && i % MAX_ROW != 0);
+        divs.push(div({ class: "hexagon-row" }, ...tmp));
+    }
+    return div(...divs); // 配列を展開してdiv要素を並べる
+};
+const DivList3 = () => {
     //1:灰,2:赤,3:黄,4:茶,5:黒,6:青,7:緑の色
     const items = [
         7, 1, 3, 4, 0, 6, 7, 1, 2, 0,
@@ -133,7 +177,7 @@ van.add(document.body, Tabs(
         },
     },
     {
-        AOI13: DivList(),
-        AOI14: div("T.B.D."),
+        AOI13: DivList3(),
+        AOI14: DivList4(),
     }
 ));
